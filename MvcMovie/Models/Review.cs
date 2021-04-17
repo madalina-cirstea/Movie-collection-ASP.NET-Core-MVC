ac978e44680f2e894e8c4ddbca7cb4caaed0fdd0
Madalina Cirstea <56949829+madalina-cirstea@users.noreply.github.com>
2021-04-17 20:04:37 +0300

    added MVC logic for Review class and migration

---
+++ MvcMovie/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        [Display(Name = "Review date")]
+        [DataType(DataType.Date)]
+        public DateTime ReviewDate { get; set; }
+    }
+}
